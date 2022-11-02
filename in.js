f433d81c468c084cff7182f058e07cec8651bc96
NGANPIN <116048192+NGANPIN@users.noreply.github.com>
2022-11-02 09:16:55 +0700

    Add files via upload

---
+++ in.js
@@ -0,0 +1,63 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  //Điền thông tin Firebase config của cá nhân vào đây
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC914I_Xqj6o6bMXz-Ar_rHP5BKoeymXrE",
+  authDomain: "kc326-ce3ba.firebaseapp.com",
+  databaseURL: "https://kc326-ce3ba-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kc326-ce3ba",
+  storageBucket: "kc326-ce3ba.appspot.com",
+  messagingSenderId: "235324032484",
+  appId: "1:235324032484:web:5dcd7a89f58b70e49cf3f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+send.addEventListener('click',(e) =>{ 
+Sends();
+});
+read.addEventListener('click',(e) =>{ 
+Getval();
+});
+
+function Sends(){
+    Number = parseFloat(document.getElementById('Number').value);
+const db = getDatabase();
+  set(ref(db, 'Number/'), {
+    Value : Number
+  });
+}
+
+function Getval(){
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `Number` + '/Value')).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        document.getElementById('val').innerHTML = "Number/Value="+snapshot.val();
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+}
